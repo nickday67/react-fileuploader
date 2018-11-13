22ab4fbde1b738a880c0f6d1cbcfe0d945f5a93a
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-11-12 20:48:38 -0400

    Add ListItem component

--- src/ListItem/ListItem.test.js
+++ src/ListItem/ListItem.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './SearchBar';
+import ListItem from './ListItem';
 
 describe('ListItem', () => {
   it('should render without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<SearchBar />, div);
+    ReactDOM.render(<ListItem/>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
