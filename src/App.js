66c81e6714477f08450fb82f39d937ac59211559
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-11-13 20:43:39 -0400

    Add search term handler

--- src/App.js
+++ src/App.js
@@ -12,12 +12,19 @@ class App extends Component {
     };
   }
 
+  updateSearchTerm(term) {
+    this.setState({
+      searchTerm: term
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <SearchBar 
           searchTerm={this.state.searchTerm} 
-          filterOption={this.state.filterOption}/>
+          filterOption={this.state.filterOption}
+          handleUpdate={term=>this.updateSearchTerm(term)}/>
         <FilterableList 
           files={this.props.files}  
           searchTerm={this.state.searchTerm} 
